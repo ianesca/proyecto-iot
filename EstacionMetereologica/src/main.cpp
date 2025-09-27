f2f7d6b00b42ac4401867b9659202c508c737a94
glalexito207 <alegutierrezlopez207@gmail.com>
2025-09-27 11:07:56 -0600

    Conexion a Internet

--- EstacionMetereologica/src/main.cpp
+++ EstacionMetereologica/src/main.cpp
@@ -1,18 +1,28 @@
 #include <Arduino.h>
+#include <WiFi.h>
 
-// put function declarations here:
-int myFunction(int, int);
+// Cambia estos valores por los de tu red Wi-Fi
+const char* ssid = "Pc-Alex";
+const char* password = "123456789";
 
 void setup() {
-  // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+  Serial.begin(115200);
+  delay(1000);
+
+  Serial.println("Conectando al WiFi...");
+  WiFi.begin(ssid, password);
+
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+
+  Serial.println();
+  Serial.println("Conexión exitosa");
+  Serial.print("Dirección IP: ");
+  Serial.println(WiFi.localIP());
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-}
-
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
+  // No es necesario hacer nada en loop
 }
